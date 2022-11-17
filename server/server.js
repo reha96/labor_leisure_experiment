9bf3839fb52839a646cde3d036b3a9d8a7c320b8
reha96 <reha.tuncer@gmail.com>
2022-11-17 16:19:17 +0100

    update policy

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
-app.use(cors());
+
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
