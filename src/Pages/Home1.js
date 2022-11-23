4b9d308ac65d2e66db9951b6f44847118d0959c9
reha96 <reha.tuncer@gmail.com>
2022-11-23 11:14:05 +0100

    test GET

--- src/Pages/Home1.js
+++ src/Pages/Home1.js
@@ -24,6 +24,15 @@ const Home1 = () => {
   const [error, setError] = useState(null);
   // console.log(typedValue)
 
+      axios
+      .get("/api/participants")
+      .then(res => {
+        console.log("new participant added with AXIOS", res.data);
+      })
+      .catch((e) => {
+        console.log("Unable to add with AXIOS: ", e);
+      });
+
   const handleChange = async (event) => {
     setChecked(event.target.checked);
     localStorage.setItem("stop", false);
