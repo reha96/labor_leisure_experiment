4b71d0d68861467ce595c1ec9a09245d2fe0444e
reha96 <reha.tuncer@gmail.com>
2022-11-29 09:44:53 +0100

    test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:5001'
+axios.defaults.baseURL = 'http://localhost:80'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
