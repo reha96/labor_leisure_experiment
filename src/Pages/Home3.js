07f84f52bf863db4b41e0b68cc18a8ed593bd4e3
reha96 <reha.tuncer@gmail.com>
2022-11-30 11:24:39 +0100

    Update Home3.js

--- src/Pages/Home3.js
+++ src/Pages/Home3.js
@@ -10,7 +10,7 @@ const Home3 = () => {
   var input = [];
 
   const nextPage = (event) => {
-    setOpen(true);
+    // setOpen(true);
     localStorage.setItem("localcount", 0);
     localStorage.setItem("clickedOKtoswitch2", "no");
     localStorage.setItem("lastmin", 1);
@@ -19,11 +19,11 @@ const Home3 = () => {
     window.localStorage.setItem("progress", 0);
   };
 
-  const [open, setOpen] = React.useState(false);
+  // const [open, setOpen] = React.useState(false);
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+  // const handleClose = () => {
+  //   setOpen(false);
+  // };
 
   return (
     <div className="Page2">
