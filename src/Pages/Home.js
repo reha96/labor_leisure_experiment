387d3443f9f7c5621f8c91c5c879bf12a776a759
reha96 <reha.tuncer@gmail.com>
2023-06-20 18:11:49 +0200

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,10 +10,10 @@ const Home = () => {
   localStorage.setItem('ignore', false)
 
   // CHOOSE WEBSITE VERSION HERE (FIRST OR SECOND DAY)
-  localStorage.setItem("version", "second"); // SECOND
+  localStorage.setItem("version", "first"); // SECOND
 
   // CHOOSE MPL
-  localStorage.setItem("mpl", true);
+  localStorage.setItem("mpl", false);
   if (localStorage.getItem('version') === 'second') {
     localStorage.setItem("mpl", false);
   }
